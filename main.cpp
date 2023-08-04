69a64141dab5062365da74262385d6248c3cae60
quocnghia32 <88430940+quocnghia32@users.noreply.github.com>
2023-08-04 10:02:25 +0700

    Update DS in 1 hour (stolen)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "Window.h"
+#include <iostream>
 
 int main() {
     Window  window;
     window.run();
+    std::cerr << "hello";
     return 0;
 }
--- source/core/Word.h
+++ source/core/Word.h
@@ -8,7 +8,7 @@
 
 class Word {
 public:
-
+    
 };
 
 
