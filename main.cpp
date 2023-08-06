01e11e8f321fc57bff5835b20807916c7f271bca
TriSi51 <ngotrisikhtn2022@gmail.com>
2023-08-06 09:57:05 +0700

    struct word

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include "Window.h"
+//#include "Window.h"
+#include <iostream>
+#include <stdio.h>
+#include <wchar.h>
+#include <io.h>
+#include <fcntl.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <codecvt>
+using namespace std;
 
 int main() {
-    Window window;
-    window.run();
+
+    // _setmode(_fileno(stdout), _O_U16TEXT);
+    //setlocale(LC_ALL, "");
+    /*_setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);*/
+    //std::locale::global(std::locale(""));
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    
+    
+
+    wifstream wfin;
+    string filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Test.txt";
+    
+    vector<WordAV> Vdictionary = readAVdictionary(filename);
+    cout << "Done\n";
+
     return 0;
+
 }
--- source/core/Word.h
+++ source/core/Word.h
@@ -4,12 +4,147 @@
 
 #ifndef CS163_GROUP9_WORD_H
 #define CS163_GROUP9_WORD_H
+#include <bits/stdc++.h>
 
-
+#include <vector>
+using namespace std;
 class Word {
 public:
-
+    
 };
 
+struct Definition {
+    wstring meaning;
+    vector<wstring> examples;
+};
+struct Type {
+    wstring type;
+    vector<Definition> definition;
+    wstring phrase;
+};
 
+struct WordAV {
+    wstring word;
+    wstring pronounce;
+    vector<Type> worddef;
+    vector<wstring> sideexplain;
+};
+vector<WordAV> readAVdictionary(const string& filename)
+{
+    locale loc(locale(), new codecvt_utf8<wchar_t>);
+    vector<WordAV> Vdictionary;
+    wifstream wfin(filename, ios::binary);
+    wfin.imbue(loc);
+    if (!wfin) {
+        cout << "Error open Vietnamese Dictionary";
+        return Vdictionary;
+    }
+    wstring line;
+    /*const int bufferSize = 8192;
+    wchar_t buffer[bufferSize];
+
+    wfin.rdbuf()->pubsetbuf(buffer, bufferSize);*/
+    getline(wfin, line);
+    while (line.size()==1 || line[0] == L'@') {
+
+        WordAV cur;
+        if (line.size()==1)
+        {
+            getline(wfin, line);
+
+            continue;
+        }
+        else if (line[0] == L'@') {
+            wcout << line[0] << endl;
+            // word and pronounciation
+
+            size_t firstSlash = line.find('/');
+            if (firstSlash != std::string::npos) {
+
+                cur.word = line.substr(1, firstSlash - 1);
+                size_t secondSlash = line.find('/', firstSlash + 1);
+                cur.pronounce = line.substr(firstSlash + 1, secondSlash - firstSlash - 1);
+                wcout << cur.pronounce << endl;
+                wcout << cur.word << endl;
+            }
+            else {
+                cur.word = line.substr(1);
+                wcout << cur.word;
+            }
+
+            getline(wfin, line);
+
+            while (line.size() != 1)
+            {
+                if (line[0] == L'*' || line[0] == L'!') {
+                    //type
+                    wcout << line[0] << endl;
+                    Type wordtype;
+                    if (line[0] == L'*')
+                        wordtype.type = line.substr(1);
+                    else wordtype.phrase = line.substr(1);
+                    getline(wfin, line);
+                    while (line.size() != 1) {
+                        if (line[0] == L'-') {
+                            wcout << line[0] << endl;
+                            Definition def;
+                            def.meaning = line.substr(1);
+                            getline(wfin, line);
+                            while (line.size() != 1) {
+                                if (line[0] == L'=') {
+                                    wcout << line[0] << endl;
+                                    def.examples.push_back(line.substr(1));
+                                    getline(wfin, line);
+
+                                }
+                                else break;
+                            }
+
+                            wordtype.definition.push_back(def);
+                            cur.worddef.push_back(wordtype);
+
+                        }
+                        else if (line[0] == L'/' || iswalpha(line[0])) {
+                            getline(wfin, line);
+                        }
+
+
+                        else break;
+
+                    }
+
+                }
+                else if (line[0] == L'-') {
+                    Type wordtype;
+                    Definition def;
+
+                    def.meaning = line.substr(1);
+                    getline(wfin, line);
+                    while (line.size() != 1) {
+                        if (line[0] == L'=') {
+                            wcout << line[0] << endl;
+                            def.examples.push_back(line.substr(1));
+                            getline(wfin, line);
+
+                        }
+                        else break;
+                    }
+
+                    wordtype.definition.push_back(def);
+                    cur.worddef.push_back(wordtype);
+                }
+                else if (line[0] == L'/'|| line[0]==L'?'|| iswalpha(line[0]) ){
+                    getline(wfin, line);
+                }
+                else break;
+
+            }
+            Vdictionary.push_back(cur);
+           
+        }
+
+    }
+    wfin.close();
+    return Vdictionary;
+}
 #endif //CS163_GROUP9_WORD_H
