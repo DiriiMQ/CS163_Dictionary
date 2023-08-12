32a2ce7a3fea0b1d5ff1a33add189d744ff5d8d0
TriSi51 <ngotrisikhtn2022@gmail.com>
2023-08-12 19:41:44 +0700

    demand

--- source/core/Api.cpp
+++ source/core/Api.cpp
@@ -36,8 +36,21 @@ bool ApiWord::removeWord(Constants::TypeDict typeDict, std::string word) {
     return false;
 }
 
-void ApiWord::editWord(Constants::TypeDict typeDict, std::string word, std::string newDefinition) {
+void ApiWord::editWord(Constants::TypeDict typeDict, Word& word, int index, Word& replace) {
+    Dict& dictionary = MainDictionary.dicts[static_cast<int>(typeDict)];
+    dictionary.words[index].worddef = replace.worddef;
+    string filename;
+    if (typeDict == Constants::TypeDict::VI_EN) {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Anh_Vietfix.dat";
+    }
+    else if (typeDict == Constants::TypeDict::En_En) {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Anh_Anhfix.dat";
+    }
+    else {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Viet_Anhfix.dat";
 
+    }
+    writetobinaryfile(dictionary.words, filename);
 }
 
 Word ApiWord::getRandomWord(Constants::TypeDict typeDict) {
@@ -65,5 +78,29 @@ bool ApiQuiz::submitQuiz(Constants::TypeDict typeDict, QuizResponse response) {
 }
 
 void Api::resetDict(Constants::TypeDict typeDict) {
+    Dict& dictionary = MainDictionary.dicts[static_cast<int>(typeDict)];
+    
+    string filename;
+    if (typeDict == Constants::TypeDict::VI_EN) {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Anh_Viet.dat";
+    }
+    else if (typeDict == Constants::TypeDict::En_En) {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Anh_Anh.dat";
+    }
+    else {
+        filename = "G:\\year1\\Semester 2\\Cs162\\Group project\\CS163_Group9\\assets\\data\\Viet_Anh.dat";
+    }
+    vector<Word> ddictionary;
+    readbinaryfile(ddictionary, filename);
+    string newfilename;
+    size_t lastSlashPos = filename.find_last_of("/");
+    if (lastSlashPos != std::string::npos) {
 
+        string path = filename.substr(0, lastSlashPos + 1);
+        string newFilename = "Anh_Vietfix.dat";
+
+       newfilename = path + newFilename;
+    }
+
+    writetobinaryfile(ddictionary, newfilename);
 }
