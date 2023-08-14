a440ef109e72c85abf2e3716ee4b140a2740d267
QuangDuongg <thaothphu@gmail.com>
2023-08-14 12:49:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "Window.h"
 #include <iostream>
 #include "_raygui.h"
+#include <core/Dict.h>
 
 int main() {
-    Window  window;
-    window.run();
+    Dict dict = Dict();
+   int n = dict.words.size();
+   cout << n;
+   /* Window  window;
+    if (n > 0)
+    window.run();*/
+    
     return 0;
 }
