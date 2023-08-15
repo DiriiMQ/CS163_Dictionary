0ebe4743f154b60b54fef9b8c5047f3dcfbcfcc8
dirii <minhquang116@gmail.com>
2023-08-15 23:05:53 +0700

    create Fav files

---
+++ source/draw/Favorite.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dirii on 15/8/23.
+//
+
+#include "Favorite.h"
---
+++ source/draw/Favorite.h
@@ -0,0 +1,14 @@
+//
+// Created by dirii on 15/8/23.
+//
+
+#ifndef CS163_GROUP9_FAVORITE_H
+#define CS163_GROUP9_FAVORITE_H
+
+
+class Favorite {
+
+};
+
+
+#endif //CS163_GROUP9_FAVORITE_H
