1165b34779688cec698dcbb89d976879f2309572
QuangDuongg <thaothphu@gmail.com>
2023-08-18 15:53:57 +0700

    full buttonimage

--- source/Window.cpp
+++ source/Window.cpp
@@ -4,7 +4,7 @@
 
 #include "Window.h"
 
-Window::Window(Api *api) {
+Window::Window(Api* api) {
     InitWindow(Constants::Screen::SCREEN_WIDTH, Constants::Screen::SCREEN_HEIGHT, Constants::Screen::NAME);
     SetTargetFPS(Constants::Screen::FRAMES_PER_SECOND);
 
@@ -15,18 +15,18 @@ Window::Window(Api *api) {
 void Window::init() {
     this->background = LoadTextureFromImage(LoadImage(Constants::Directories::DMQ::BG));
     this->font = LoadFontEx(
-            Constants::Screen::FONT,
-            32,
-            nullptr,
-            40000
+        Constants::Screen::FONT,
+        32,
+        nullptr,
+        40000
     );
     this->searchBox = SearchBox(30, { 92.5, 155.3, 690.7, 66.1 }, &font);
     for (int i = (int)Constants::Screen::menuBtn::WORD; i != (int)Constants::Screen::menuBtn::NONE; ++i) {
         this->menuButtons[i] = Button(
-                Constants::Screen::NAME_MENU_BTN[i],
-                20,
-                BLUE,
-                Constants::Screen::RECT_MENU_BTN[i]
+            Constants::Screen::NAME_MENU_BTN[i],
+            20,
+            BLUE,
+            Constants::Screen::RECT_MENU_BTN[i]
         );
     }
 
@@ -34,41 +34,41 @@ void Window::init() {
 
     for (int i = (int)Constants::Screen::operationBtn::REMOVE; i != (int)Constants::Screen::operationBtn::NONE; ++i) {
         this->operationButtons[i] = Button(
-                Constants::Screen::NAME_OPERATION_BTN[i],
-                20,
-                BLUE,
-                Constants::Screen::RECT_OPERATION_BTN[i]
+            Constants::Screen::NAME_OPERATION_BTN[i],
+            20,
+            BLUE,
+            Constants::Screen::RECT_OPERATION_BTN[i]
         );
     }
 
     this->saveButton = Button(
-            Constants::Screen::NAME_SAVE_BTN,
-            20,
-            BLUE,
-            Constants::Screen::RECT_SAVE_BTN
+        Constants::Screen::NAME_SAVE_BTN,
+        20,
+        BLUE,
+        Constants::Screen::RECT_SAVE_BTN
     );
 
     this->resetButton = Button(
-            Constants::Screen::NAME_RESET_BTN,
-            20,
-            BLUE,
-            Constants::Screen::RECT_RESET_BTN
+        Constants::Screen::NAME_RESET_BTN,
+        20,
+        BLUE,
+        Constants::Screen::RECT_RESET_BTN
     );
 
     this->activeMenu = (int)Constants::Screen::menuBtn::NONE;
     this->activeOperation = (int)Constants::Screen::operationBtn::NONE;
 
-    this->mainInfoBG = { 47,259,810,420};
+    this->mainInfoBG = { 47,259,810,420 };
 
     this->frameBoard = FrameBoard(
-            {
-                this->mainInfoBG.x + 10,
-                this->mainInfoBG.y + 10,
-                this->mainInfoBG.width - 20,
-                this->mainInfoBG.height - 20
-                },
-            &this->font
-            );
+        {
+            this->mainInfoBG.x + 10,
+            this->mainInfoBG.y + 10,
+            this->mainInfoBG.width - 20,
+            this->mainInfoBG.height - 20
+        },
+        &this->font
+    );
     // For init Favourite
     this->favourite = Favourite(&font, (int)currentDict, api);
     this->currentSearch = " ";
@@ -103,7 +103,7 @@ void Window::run() {
 
 void Window::draw() {
     DrawTexture(background, 0, 0, WHITE);
-    for (auto & menuButton : this->menuButtons) {
+    for (auto& menuButton : this->menuButtons) {
         menuButton.draw();
     }
 
@@ -112,19 +112,25 @@ void Window::draw() {
         this->frameBoard.draw();
 
         if (this->activeMenu != (int)Constants::Screen::menuBtn::FAVOURITE) {
-            for (auto & operationButton : this->operationButtons) {
+            for (auto& operationButton : this->operationButtons) {
                 operationButton.draw();
+            }
+            if (this->isShowingWord) {
+                // draw star here
+
             }
             this->searchBox.draw();
             if (this->activeOperation == (int)Constants::Screen::operationBtn::ADD || this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
                 this->saveButton.draw();
             }
-        } else {
+        }
+        else {
             // Draw favourite from DMQ
             this->favourite.draw();
             this->saveButton.draw();
         }
     }
+
     // For Quiz & Type of Dict
     this->DataSwitchButton.draw();
     this->QuizButton.draw();
@@ -133,23 +139,36 @@ void Window::draw() {
 }
 
 void Window::handleEvents() {
-    for (auto & menuButton : this->menuButtons) {
+    for (auto& menuButton : this->menuButtons) {
         menuButton.handleEvents();
     }
+
+    // For Quiz & Type of Dict 
+    this->DataSwitchButton.handleEvents();
+    this->QuizButton.handleEvents();
+    currentDict = (Constants::TypeDict)this->DataSwitchButton.getClicked();
+
     this->resetButton.handleEvents();
 
     if (this->activeMenu != (int)Constants::Screen::menuBtn::NONE) {
         this->frameBoard.handleEvents();
         if (this->activeMenu != (int)Constants::Screen::menuBtn::FAVOURITE) {
-            for (auto &operationButton: this->operationButtons) {
+            for (auto& operationButton : this->operationButtons) {
                 operationButton.handleEvents();
             }
+
+            if (this->isShowingWord) {
+                // handle star here
+
+            }
+
             this->searchBox.handleEvents();
 
             if (this->activeOperation == (int)Constants::Screen::operationBtn::ADD || this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
                 this->saveButton.handleEvents();
             }
-        } else {
+        }
+        else {
             // Handle favourite from DMQ
             this->favourite.handleEvents();
             this->saveButton.handleEvents();
@@ -158,13 +177,18 @@ void Window::handleEvents() {
 }
 
 void Window::update() {
-//    std::cout << "Active menu: " << this->activeMenu << std::endl;
-//    std::cout << "Active operation: " << this->activeOperation << std::endl;
+    //    std::cout << "Active menu: " << this->activeMenu << std::endl;
+    //    std::cout << "Active operation: " << this->activeOperation << std::endl;
     this->handleEvents();
 
-    for (auto & menuButton : this->menuButtons) {
+    for (auto& menuButton : this->menuButtons) {
         menuButton.update();
     }
+
+    // For Quiz & Type of Dict (remember to set this->activeMenu to NONE when click on Quiz)
+    this->DataSwitchButton.update();
+    this->QuizButton.update();
+
     this->resetButton.update();
 
     for (int i = 0; i < 3; ++i) {
@@ -172,13 +196,14 @@ void Window::update() {
             std::cout << "LOG: Menu button " << i << " is clicked" << std::endl;
             this->menuButtons[i].setChosen(true);
             this->activeMenu = i;
+            this->isShowingWord = false;
             for (int j = 0; j < 3; ++j) {
                 if (j != i) {
                     this->menuButtons[j].setChosen(false);
                 }
                 this->operationButtons[j].setChosen(false);
-                this->activeOperation = (int)Constants::Screen::operationBtn::NONE;
             }
+            this->activeOperation = (int)Constants::Screen::operationBtn::NONE;
             this->frameBoard.reset();
             break;
         }
@@ -187,14 +212,15 @@ void Window::update() {
     if (this->activeMenu != (int)Constants::Screen::menuBtn::NONE) {
         if (this->activeMenu != (int)Constants::Screen::menuBtn::FAVOURITE) {
             this->updateModeNonFavorite();
-        } else {
+        }
+        else {
             // Update favourite from DMQ
-           
-            
+            //  this->favourite.update();
+
 
             this->saveButton.update();
             if (this->saveButton.isClicked()) {
-//                this->saveFrameBoard();
+                //                this->saveFrameBoard();
                 this->favourite.removeWhenSave();
                 this->favourite.update();
             }
@@ -217,20 +243,25 @@ void Window::reset() {
     this->frameBoard.reset();
     this->activeMenu = (int)Constants::Screen::menuBtn::NONE;
     this->activeOperation = (int)Constants::Screen::operationBtn::NONE;
+    // For Quiz and DataSwitch
+    this->DataSwitchButton.changeIndex(0);
+    this->QuizButton.changeIndex(0);
 }
 
 void Window::updateOperationButtons() {
     if (this->activeOperation == (int)Constants::Screen::operationBtn::ADD) {
-//        std::cout << "LOG: WordAdd: " << this->wordAdd << std::endl;
-    } else if (this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
+        //        std::cout << "LOG: WordAdd: " << this->wordAdd << std::endl;
+    }
+    else if (this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
 
-    } else if (this->activeOperation == (int)Constants::Screen::operationBtn::REMOVE) {
+    }
+    else if (this->activeOperation == (int)Constants::Screen::operationBtn::REMOVE) {
 
     }
 }
 
 void Window::updateModeNonFavorite() { // Update for Search Mode
-    for (auto &operationButton: this->operationButtons)
+    for (auto& operationButton : this->operationButtons)
         operationButton.update();
     if (this->activeOperation == (int)Constants::Screen::operationBtn::ADD || this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
         this->saveButton.update();
@@ -244,16 +275,18 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
         if (this->operationButtons[i].isClicked()) {
             std::cout << "LOG: Operation button " << i << " is clicked" << std::endl;
             this->frameBoard.reset();
+            this->isShowingWord = false;
             if (i == (int)Constants::Screen::operationBtn::ADD) {
                 this->frameBoard.setBlocks({
                     {"Word: ", &this->wordAdd},
                     {"Definition: ", &this->definitionAdd}
-                });
-                this->frameBoard.setEditLines({0, 1});
-            } else if (i == (int)Constants::Screen::operationBtn::EDIT) {
+                    });
+                this->frameBoard.setEditLines({ 0, 1 });
+            }
+            else if (i == (int)Constants::Screen::operationBtn::EDIT) {
                 // set perm edit
                 std::vector<std::pair<std::string, std::string*>> _blocks;
-                for (auto &i : this->lines) {
+                for (auto& i : this->lines) {
                     _blocks.emplace_back("", &i.first);
                 }
                 this->frameBoard.setBlocks(_blocks);
@@ -266,10 +299,11 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
                 }
                 this->frameBoard.setEditLines(editLines);
 
-            } else if (i == (int)Constants::Screen::operationBtn::REMOVE) {
+            }
+            else if (i == (int)Constants::Screen::operationBtn::REMOVE) {
                 this->frameBoard.setBlocks({
                     {"", &this->REMOVE_NOTICE}
-                });
+                    });
                 this->api->apiWord.removeWord(this->currentDict, this->currentWord.word);
                 this->currentWord = Word();
             }
@@ -284,6 +318,11 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
         }
     }
 
+    // for Star
+    if (this->isShowingWord) {
+
+    }
+
     // Search Box
     this->searchBox.update();
 
@@ -294,13 +333,15 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
         std::cout << "LOG: Search text: " << _searchText << std::endl;
 
         // call Api and update list
-        std::vector<std::pair<std::string, std::string>> &_list = this->suggestListText;
+        std::vector<std::pair<std::string, std::string>>& _list = this->suggestListText;
         std::wstring curWstr = Utils::UTF8ToWString(_searchText);
         std::wcout << "LOG: curWstr: " << curWstr << std::endl;
         this->_wordList.clear();
         if (_searchText.empty()) {
+            std::cout << "LOG: get history" << std::endl;
             this->_wordList = this->api->apiSearch.getHistory(this->currentDict);
-        } else {
+        }
+        else {
             if (this->activeMenu == (int)Constants::Screen::menuBtn::WORD)
                 this->_wordList = this->api->apiSearch.getAutoCompleteListForWord(this->currentDict, curWstr);
             if (this->activeMenu == (int)Constants::Screen::menuBtn::DEFINITION)
@@ -310,8 +351,8 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
         _list.clear();
         for (auto i : _wordList) {
             _list.emplace_back(
-                    Utils::WStringToUTF8(i),
-                    ""
+                Utils::WStringToUTF8(i),
+                ""
             );
         }
 
@@ -323,6 +364,7 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
     if (choice != -1) {
         std::cout << "LOG: Search box choice: " << choice << std::endl;
         // get tu list ra lay result show nguoc ra frameBoard
+        this->isShowingWord = true;
         this->frameBoard.reset();
         this->currentWord = this->api->apiWord.getWord(this->currentDict, this->_wordList[choice]);
         std::wcout << "LOG: currentWord: " << this->currentWord.word << std::endl;
@@ -335,32 +377,32 @@ void Window::updateModeNonFavorite() { // Update for Search Mode
 void Window::createLines() {
     this->lines.clear();
 
-    Word &cur = this->currentWord;
+    Word& cur = this->currentWord;
     this->lines.push_back({
-        Utils::WStringToUTF8(cur.word) + (cur.pronounce != L"Null" ? ("  /" + Utils::WStringToUTF8(cur.pronounce) + "/") : ""), 
+        Utils::WStringToUTF8(cur.word) + (cur.pronounce != L"Null" ? ("  /" + Utils::WStringToUTF8(cur.pronounce) + "/") : ""),
         false
-    });
+        });
 
-    for (auto &i : cur.worddef) {
-        this->lines.push_back({"________", false});
+    for (auto& i : cur.worddef) {
+        this->lines.push_back({ "________", false });
         if (i.type != L"Null")
-            this->lines.push_back({Utils::WStringToUTF8(i.type), true});
-        
-        for (auto &j : i.definition) {
-            this->lines.push_back({Utils::WStringToUTF8(j.meaning), true});
+            this->lines.push_back({ Utils::WStringToUTF8(i.type), true });
+
+        for (auto& j : i.definition) {
+            this->lines.push_back({ Utils::WStringToUTF8(j.meaning), true });
             if (j.Isexample) {
-                for (auto &k : j.examples) {
-                    this->lines.push_back({Utils::WStringToUTF8(k), true});
+                for (auto& k : j.examples) {
+                    this->lines.push_back({ Utils::WStringToUTF8(k), true });
                 }
             }
         }
 
         if (i.phrase != L"Null")
-            this->lines.push_back({Utils::WStringToUTF8(i.phrase), true});
+            this->lines.push_back({ Utils::WStringToUTF8(i.phrase), true });
     }
 
     std::vector<std::pair<std::string, std::string*>> _blocks;
-    for (auto &i : this->lines) {
+    for (auto& i : this->lines) {
         _blocks.emplace_back("", &i.first);
     }
     this->frameBoard.setBlocks(_blocks);
@@ -381,8 +423,8 @@ void Window::saveFrameBoard() {
     }
     if (this->activeOperation == (int)Constants::Screen::operationBtn::EDIT) {
         std::cout << "LOG: Edit" << std::endl;
-        
-        Word &cur = this->currentWord;
+
+        Word& cur = this->currentWord;
         this->api->apiWord.removeWord(this->currentDict, cur.word);
         // this->lines.push_back({
         //     Utils::WStringToUTF8(cur.word) + (cur.pronounce != L"Null" ? ("  /" + Utils::WStringToUTF8(cur.pronounce) + "/") : ""), 
@@ -390,22 +432,22 @@ void Window::saveFrameBoard() {
         // });
         int counter = 0;
 
-        for (auto &i : cur.worddef) {
+        for (auto& i : cur.worddef) {
             // this->lines.push_back({"________", false});
             ++counter;
 
-            if (i.type != L"Null"){
+            if (i.type != L"Null") {
                 // this->lines.push_back({Utils::WStringToUTF8(i.type), true});
                 ++counter;
                 i.type = Utils::UTF8ToWString(this->lines[counter].first);
             }
-            
-            for (auto &j : i.definition) {
+
+            for (auto& j : i.definition) {
                 // this->lines.push_back({Utils::WStringToUTF8(j.meaning), true});
                 ++counter;
                 j.meaning = Utils::UTF8ToWString(this->lines[counter].first);
                 if (j.Isexample) {
-                    for (auto &k : j.examples) {
+                    for (auto& k : j.examples) {
                         // this->lines.push_back({Utils::WStringToUTF8(k), true});
                         ++counter;
                         k = Utils::UTF8ToWString(this->lines[counter].first);
@@ -413,7 +455,7 @@ void Window::saveFrameBoard() {
                 }
             }
 
-            if (i.phrase != L"Null"){
+            if (i.phrase != L"Null") {
                 // this->lines.push_back({Utils::WStringToUTF8(i.phrase), true});
                 ++counter;
                 i.phrase = Utils::UTF8ToWString(this->lines[counter].first);
