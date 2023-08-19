c494e1542b700213c9c0ae381ca791967dc13f69
QuangDuongg <thaothphu@gmail.com>
2023-08-20 00:51:07 +0700

    updt main test

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@
 int main() {
     Dicts dicts;
     Api api(dicts);
-    Window window(&api);
-    vector <wstring> s = api.apiSearch.getAutoCompleteListForWord((Constants::TypeDict)3, L"f");
-    wcout << s.size();
-    window.run();
+    Constants::TypeDict typedict = (Constants::TypeDict) 0;
+    api.apiFavorite.resetFavorite(typedict);
+    api.apiFavorite.addFavorite(typedict, L"Hi");
+    api.apiFavorite.addFavorite(typedict, L"Hello");
+    api.apiFavorite.removeFavorite(typedict, L"Hi");
+    vector<wstring>tmp = api.apiFavorite.getFavorite(typedict);
+    for (int i = 0; i < tmp.size(); i++)
+        wcout << tmp[i];
+   // Window window(&api);
+  /*  vector <wstring> s = api.apiSearch.getAutoCompleteListForWord((Constants::TypeDict)3, L"f");
+    wcout << s.size();*/
+   // window.run();
     return 0;
 }
